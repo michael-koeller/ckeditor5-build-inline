0e62b44b01194972a523f168bca859db50daff21
Michael <michael.koeller@greenbytes.de>
2019-03-08 12:53:35 +0100

    Implement datafield plugin

--- plugins/datafield/src/datafield.js
+++ plugins/datafield/src/datafield.js
@@ -1,9 +1,30 @@
 /* global console */
+
+/**
+ * @module datafield
+ */
+
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+
+import '../theme/datafield.css';
+
+const DATAFIELD = 'datafield';
 
 export default class DataField extends Plugin {
 	init() {
 		console.log( 'DataField plugin was initialized' );
+
+		const editor = this.editor;
+		// Allow datafield attribute on text nodes.
+		editor.model.schema.extend( '$text', { allowAttributes: DATAFIELD } );
+
+		editor.conversion.attributeToElement( {
+			model: DATAFIELD,
+			view: {
+				name: 'span',
+				classes: 'datafield'
+			}
+		} );
 	}
 
 	static get pluginName() {
